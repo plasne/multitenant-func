f4875e02a7f94c39dc75ecceeea0cc20f2492346
plasne <plasne@Peters-iMac.local>
2016-04-01 16:07:10 -0400

    updated

--- testauth/index.js
+++ testauth/index.js
@@ -1,8 +1,15 @@
 module.exports = function(context, req) {
-    
-    context.log(req);
- 
-   
-    context.log("testing");
+    if (req.headers.authorization) {
+        var token = req.headers.authorization.replace("Bearer ", "");
+        context.res = {
+            status: 200,
+            body: { message: "greetings guy" }
+        }
+    } else {
+        context.res = {
+            status: 401,
+            body: "Unauthorized: no access token was passed."
+        };
+    }
     context.done();
 };
