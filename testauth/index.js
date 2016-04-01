c70d78a1b0dd0fd45014a386857ccc1f6d18c369
plasne <plasne@Peters-iMac.local>
2016-04-01 16:24:34 -0400

    ex

--- testauth/index.js
+++ testauth/index.js
@@ -4,24 +4,28 @@ module.exports = function(context, req) {
     if (req.headers.authorization) {
         var token = req.headers.authorization.replace("Bearer ", "");
         var jwtKey = process.env.jwtKey;
-        nJwt.verify(token, jwtKey, function(err, verified) {
-            context.log("here-01");
-            if (err) {
-                context.log(err);
-                context.log("here-03");
-                context.res = {
-                    status: 401,
-                    body: "Unauthorized: the access token could not be verified."
+        try {
+            nJwt.verify(token, jwtKey, function(err, verified) {
+                context.log("here-01");
+                if (err) {
+                    context.log(err);
+                    context.log("here-03");
+                    context.res = {
+                        status: 401,
+                        body: "Unauthorized: the access token could not be verified."
+                    }
+                } else {
+                    context.log("here-02");
+                    context.log(verified.body.sub);
+                    context.res = {
+                        status: 200,
+                        body: { message: "greetings " + verified.body.sub }
+                    }
                 }
-            } else {
-                context.log("here-02");
-                context.log(verified.body.sub);
-                context.res = {
-                    status: 200,
-                    body: { message: "greetings " + verified.body.sub }
-                }
-            }
-        });
+            });
+        } catch(e) {
+            context.log(e);
+        }
     } else {
         context.res = {
             status: 401,
