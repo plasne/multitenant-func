41efe600d564d2699ba02a1a1e5414eb42dd53bb
plasne <plasne@Peters-iMac.local>
2016-04-01 16:23:03 -0400

    here

--- testauth/index.js
+++ testauth/index.js
@@ -4,16 +4,18 @@ module.exports = function(context, req) {
     if (req.headers.authorization) {
         var token = req.headers.authorization.replace("Bearer ", "");
         var jwtKey = process.env.jwtKey;
-        context.log(process.env);
-        context.log(jwtKey);
         nJwt.verify(token, jwtKey, function(err, verified) {
+            context.log("here-01");
             if (err) {
                 context.log(err);
+                context.log("here-03");
                 context.res = {
                     status: 401,
                     body: "Unauthorized: the access token could not be verified."
                 }
             } else {
+                context.log("here-02");
+                context.log(verified.body.sub);
                 context.res = {
                     status: 200,
                     body: { message: "greetings " + verified.body.sub }
