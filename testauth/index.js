4ba07b29382ac8887a23eb88ff16bd3a15c04845
plasne <plasne@Peters-iMac.local>
2016-04-01 16:45:08 -0400

    full response

--- testauth/index.js
+++ testauth/index.js
@@ -3,14 +3,29 @@ var nJwt = require("njwt");
 module.exports = function(context, req) {
     if (req.headers.authorization) {
         try {
+            
+            // verify the token
             var token = req.headers.authorization.replace("Bearer ", "");
             var jwtKey = process.env.jwtKey;
             var verified = nJwt.verify(token, jwtKey);
-            context.log(verified.body.sub);
+
+            // build the response
+            var role = "none";
+            if (verified.body.scope) {
+                if (verified.body.scope.indexOf("users") > -1) role = "user";
+                if (verified.body.scope.indexOf("admins") > -1) role = "admin";
+            }
+            
+            // respond
             context.res = {
                 status: 200,
-                body: { message: "greetings " + verified.body.sub }
-            }
+                body: {
+                    "id": verified.body.sub,
+                    "role": role,
+                    "rights": (verified.body.rights) ? verified.body.rights : "none"
+                }
+            };
+            
         } catch(e) {
             context.res = {
                 status: 401,
